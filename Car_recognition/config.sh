3f733df398da493731e62b69119ab31140c669f2
yizhitooz <uzalwong@outlook.com>
2024-04-21 19:41:00 +0800

    更改结构

--- Car_recognition/config.sh
+++ Car_recognition/config.sh
@@ -1,2 +1,5 @@
-pip install -r requirements.txt
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+python -V # 检查Python是否安装好
+python -m venv venv # 创建虚拟环境
+venv\Scripts\activate # 进入虚拟环境
+pip install -r requirements.txt # 安装需要的软件包
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 # 安装torch
