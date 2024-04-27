37f55c72805b96627b7b0ccbcbd53421f17fb2a5
yizhitooz <uzalwong@outlook.com>
2024-04-28 01:16:47 +0800

    Qt 完成用户类

--- ipms_Qt/User.h
+++ ipms_Qt/User.h
@@ -1,4 +1,27 @@
 ﻿#ifndef USER_H
 #define USER_H
 
+#include <QString>
+
+class User
+{
+public:
+    User();
+
+    // 设置和获取用户名
+    QString getUsername() const;
+    void setUsername(const QString &username);
+
+    // 设置和获取密码
+    QString getPassword() const;
+    void setPassword(const QString &password);
+
+    // 其他可能的用户信息和操作
+
+private:
+    QString m_username;
+    QString m_password;
+    // 其他可能的用户属性
+};
+
 #endif // USER_H
